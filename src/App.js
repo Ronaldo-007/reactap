2d75ca0bf63c14dd65312682990a2c332ef2d6ce
Ronaldo-007 <azizjonaxmadaliyev@gmail.com>
2021-07-11 13:07:43 +0500

    cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="bg-white shadow">
+              <img src="https://image.freepik.com/free-photo/woman-red-apron-decorates-with-colored-sprinkles-cupcakes-kitchen-home_119439-741.jpg" className="w-100 mb-3" alt="" />
+              <h1 className="mx-4">Bjkhdkgh jbdjs vbdkjfbvj</h1>
+              <p className="text-secondary mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque dolorem ullam, non quis sapiente numquam repellendus id placeat beatae maxime optio aliquid hic corporis eligendi velit animi quam sunt.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="bg-white shadow">
+              <img src="https://image.freepik.com/free-photo/woman-red-apron-decorates-with-colored-sprinkles-cupcakes-kitchen-home_119439-741.jpg" className="w-100 mb-3" alt="" />
+              <h1 className="mx-4">Bjkhdkgh jbdjs vbdkjfbvj</h1>
+              <p className="text-secondary mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque dolorem ullam, non quis sapiente numquam repellendus id placeat beatae maxime optio aliquid hic corporis eligendi velit animi quam sunt.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+           <div className="bg-white shadow">
+           <img src="https://image.freepik.com/free-photo/woman-red-apron-decorates-with-colored-sprinkles-cupcakes-kitchen-home_119439-741.jpg" className="w-100 mb-3" alt="" />
+            <h1 className="mx-4">Bjkhdkgh jbdjs vbdkjfbvj</h1>
+            <p className="text-secondary mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque dolorem ullam, non quis sapiente numquam repellendus id placeat beatae maxime optio aliquid hic corporis eligendi velit animi quam sunt.</p>
+           </div>
+          </div>
+        </div>
+      </div>
 
     </div >
   );
